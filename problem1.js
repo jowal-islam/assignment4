b64ef15a9dc36cfcb4c357ff5e3c422a94b68e6c
jowal-islam <ranabrindabon@gmail.com>
2025-08-14 18:17:12 +0600

    2problem solved

---
+++ problem1.js
@@ -0,0 +1,32 @@
+
+// problem- 01
+
+// function totalFine( fare ) {
+   
+//     if(fare <= 0 || typeof fare !== "number"){
+//        return "Invalid"
+//     } else {
+//        let fine = fare + fare * (20 / 100) + 30; 
+//         return fine;
+//     }
+      
+// }
+
+// problem - 02
+
+
+// function  onlyCharacter( str ) {
+//          if(typeof str !== "string"){
+//             return "Invalid";
+//          } else {
+//            return str.replaceAll(' ', '').toUpperCase();
+//          }
+// }
+
+
+
+
+   
+
+
+
